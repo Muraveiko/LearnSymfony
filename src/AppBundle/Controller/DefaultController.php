3b8dd460f26055b5a96c1011a55bea6664c0723d
Muraveiko <oleg@muraveyko.ru>
2016-12-12 01:28:42 +0300

    Получилась страшненькая форма добавления

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,16 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Form\BookType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Response;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\Book;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 
 class DefaultController extends Controller
 {
@@ -21,4 +28,51 @@ class DefaultController extends Controller
             'books' => $books
         ]);
     }
+
+
+    /**
+     * Creates a new Book entity.
+     *
+     * @Route("/new", name="add_book")
+     * @Method({"GET", "POST"})
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book)
+            ->add('saveAndCreateNew', SubmitType::class);
+
+        $form->handleRequest($request);
+
+        // the isSubmitted() method is completely optional because the other
+        // isValid() method already checks whether the form is submitted.
+        // However, we explicitly add it to improve code readability.
+        // See http://symfony.com/doc/current/best_practices/forms.html#handling-form-submits
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($book);
+            $entityManager->flush();
+
+            // Flash messages are used to notify the user about the result of the
+            // actions. They are deleted automatically from the session as soon
+            // as they are accessed.
+            // See http://symfony.com/doc/current/book/controller.html#flash-messages
+            $this->addFlash('success', 'post.created_successfully');
+
+            if ($form->get('saveAndCreateNew')->isClicked()) {
+                return $this->redirectToRoute('add_book');
+            }
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('book/new.html.twig', [
+            'post' => $book,
+            'form' => $form->createView(),
+        ]);
+
+    }
+
 }

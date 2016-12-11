b3f88fd12c50b09dfb23a35866be24d32dab236c
Muraveiko <oleg@muraveyko.ru>
2016-12-11 20:59:52 +0300

    Создали сущность для книг

---
+++ src/AppBundle/Entity/Book.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table(name="book")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\BookRepository")
+ */
+class Book
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=128)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cover", type="string", length=45)
+     */
+    private $cover;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="filename", type="string", length=255)
+     */
+    private $filename;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_read", type="date")
+     */
+    private $dateRead;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="allowed_download", type="boolean")
+     */
+    private $allowedDownload;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Book
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     *
+     * @return Book
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set cover
+     *
+     * @param string $cover
+     *
+     * @return Book
+     */
+    public function setCover($cover)
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    /**
+     * Get cover
+     *
+     * @return string
+     */
+    public function getCover()
+    {
+        return $this->cover;
+    }
+
+    /**
+     * Set filename
+     *
+     * @param string $filename
+     *
+     * @return Book
+     */
+    public function setFilename($filename)
+    {
+        $this->filename = $filename;
+
+        return $this;
+    }
+
+    /**
+     * Get filename
+     *
+     * @return string
+     */
+    public function getFilename()
+    {
+        return $this->filename;
+    }
+
+    /**
+     * Set dateRead
+     *
+     * @param \DateTime $dateRead
+     *
+     * @return Book
+     */
+    public function setDateRead($dateRead)
+    {
+        $this->dateRead = $dateRead;
+
+        return $this;
+    }
+
+    /**
+     * Get dateRead
+     *
+     * @return \DateTime
+     */
+    public function getDateRead()
+    {
+        return $this->dateRead;
+    }
+
+    /**
+     * Set allowedDownload
+     *
+     * @param boolean $allowedDownload
+     *
+     * @return Book
+     */
+    public function setAllowedDownload($allowedDownload)
+    {
+        $this->allowedDownload = $allowedDownload;
+
+        return $this;
+    }
+
+    /**
+     * Get allowedDownload
+     *
+     * @return bool
+     */
+    public function getAllowedDownload()
+    {
+        return $this->allowedDownload;
+    }
+}
+
